cae9fd16e1d44c1d0e89c208ce4daefd87f55aa3
aiman <abduldanmaihaja@gmail.com>
2022-06-17 14:56:12 +0100

    api to get countries using euros

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const renderCountry = (data, className = '') => {
+  const html = `   <article class="country ${className}">
+ <img class="country__img" src="${data.flags.png}" />
+ <div class="country__data">
+ <h3 class="country__name">${data.name.common}</h3>
+ <h4 class="country__region">${data.region}</h4>
+ <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
+   1
+ )} ${'people'}</p>
+  <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+  <p class="country__row"><span>💰</span>${data.currencies.EUR.name} ${
+    data.currencies.EUR.symbol
+  }</p>
+ </div>
+</article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 const country = country => {
   const request = new XMLHttpRequest();
   request.open(
@@ -15,23 +33,23 @@ const country = country => {
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
-
-    const html = `   <article class="country">
-  <img class="country__img" src="${data.flags.png}" />
-  <div class="country__data">
-  <h3 class="country__name">${data.name.common}</h3>
-  <h4 class="country__region">${data.region}</h4>
-  <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(
-    1
-  )} ${'people'}</p>
-   <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
-   <p class="country__row"><span>💰</span>${data.currencies.NGN.name} ${
-      data.currencies.NGN.symbol
-    }</p>
-  </div>
-</article>`;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    renderCountry(data);
+    const neighbour = data.borders?.[0];
+    console.log(neighbour);
+    const request2 = new XMLHttpRequest();
+    request2.open(
+      'GET',
+      `
+      https://restcountries.com/v3.1/alpha/${neighbour}`
+    );
+    request2.send();
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
-country('nigeria');
+country('portugal');
+// country('germany');
+// country('france');
